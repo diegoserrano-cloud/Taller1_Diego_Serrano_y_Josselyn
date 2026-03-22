8c942e28e781d4ab3e885035313c79ad248ab7e6
josselynbarraza-sys <josselyn.barraza@alumnos.ucn.cl>
2026-03-22 17:01:12 -0300

    Implement user authentication with file input

--- Taller_1/logica/Main.java
+++ Taller_1/logica/Main.java
@@ -11,9 +11,29 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		File Tusuarios = new File("C:\\Users\\Alumno\\eclipse-workspace\\Taller_1\\usuarios");
 		
-		
-		
+		Scanner lector = new Scanner(Tusuarios);// lee las lineas
+		String[] nombres= {"", "", ""};
+		String[] contraseñas= {"", "", ""};
+		int i = 0;
+		while (lector.hasNextLine()) { // lee el archivo
+			String linea = lector.nextLine();
+			String[] partes = linea.split(";");
+			String nombrer= partes[0];
+			nombres[i] = nombrer;
+			String contraseña = partes[1];
+			contraseñas[i] = contraseña;
+			if (i < 3) {
+				++i;}
+		}
+		int a;
+		for (a = 0; a< nombres.length; a++){ // esto solo es para guiarnos con las contraseñas despues se eliminara
+			System.out.print(nombres[a]+ " ");
+			System.out.print(contraseñas[a]+ " ");
+			System.out.println();
+		}
+
 		Scanner opcion = new Scanner(System.in);
 		
 		int op;
@@ -46,22 +66,45 @@ public class Main {
             	opcion.nextLine();
             	System.out.print("Usuario: ");
             	String nombre = opcion.nextLine();// segun el nombre que nos den usamos la contraseña
-            	
+            	Boolean condicion = false;
             	if (nombre.equalsIgnoreCase("Martin")) {
             		System.out.print("Contraseña: ");
-            		String contraseña_Martin = opcion.nextLine(); 
-            		
+            		String contraseña_Martin = opcion.nextLine();
+					if (contraseña_Martin.equals(contraseñas[0])) {
+            			condicion = true;
+            			System.out.println();
+					}
             	}else if (nombre.equalsIgnoreCase("Catalina")) {
             		System.out.print("Contraseña: ");
             		String contraseña_Catalina = opcion.nextLine(); 
-            		
+					if (contraseña_Catalina.equals(contraseñas[1])) {
+            			condicion = true;
+            			System.out.println();
+					}
             	}else if (nombre.equalsIgnoreCase("Estefania")) {
             		System.out.print("Contraseña: ");
             		String contraseña_Estefania = opcion.nextLine(); 
+					if (contraseña_Estefania.equals(contraseñas[2])) {
+            			condicion = true;
+            			System.out.println();
+					}	
             	}else {
             		System.out.println("Usuario invalido");
             		System.out.println();
-            	}	
+            	}if(condicion) {
+            		System.out.println("Acceso correcto!");
+            		System.out.println();
+            		System.out.println("Bienvenido"+ " " + nombre + "!");
+            		System.out.println();
+            		System.out.println("Que deseas realizar?");
+            		System.out.println();
+            		System.out.println("1) Registrar actividad. " 
+        					+ "\n2) Modificar actividad." 
+        					+ "\n3) Eliminar actividad."
+        					+ "\n4) Cambiar contraseña."
+        					+ "\n5) Salir.");
+            		System.out.println();
+				}
 			}
 		} while (op != 3);
 			
